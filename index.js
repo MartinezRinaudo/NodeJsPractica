201bf94dcfcc027863be09576a7c9ac2b418d9a0
MartinezRinaudo <84363516+MartinezRinaudo@users.noreply.github.com>
2023-03-07 13:03:04 -0300

    #1 Adding server

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-// run `node index.js` in the terminal
+var http = require('http');
+var code = 200;
+var optionHttp = { 'Content-Type': 'application/json'};
+var port = 8080;
+var message = "Hello World"
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+var server = http.createServer((req,res) => {
+  res.writeHead(code, optionHttp);
+  res.end(message);
+});
+
+server.listen(port);
